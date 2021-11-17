4026298660a5a05d7afc5243f53eec43d3089a47
Marcelo de Jesus Nunez <chelo154@gmail.com>
2021-11-17 19:08:20 -0300

    Fixed support command

--- src/bot/commands/support.py
+++ src/bot/commands/support.py
@@ -11,17 +11,22 @@ def load_support(bot: Bot):
 
 
 def __build_embed__(bot: Bot) -> Embed:
-    embed = Embed(title='Support Project!!', description='Thank you for support this project, you can make a donation '
-                                                         'via ceneka', color=Color.gold())
+    embed = Embed(title='Support Project!!', description='Thank you for supporting this project, you can make a '
+                                                         'donation via ceneka', color=Color.gold())
 
-    embed.set_author(name='cheavlo154', icon_url='https://i.ibb.co/QM960vb/Perfil.jpg', url='github.com/chelo154')
+    embed.set_author(name='chelo154', icon_url='https://i.ibb.co/QM960vb/Perfil.jpg', url='https://github.com'
+                                                                                          '/chelo154')
 
-    embed.add_field(name='Developed by', value='@chelo154 (github.com/chelo154)')
+    embed.add_field(name='Developed by', value='@chelo154 -> https://github.com/chelo154')
 
-    embed.add_field(name='Support', value='You can donate via ceneka clicking here -> https://ceneka.net/chelo154')
+    embed.add_field(name='Support',
+                    value='You can donate via ceneka clicking here -> https://ceneka.net/chelo154',
+                    inline=False)
 
-    embed.add_field(name='Contact', value='For feedback and new ideas you can contact me via e-mail '
-                                          'mdjnunez9706@gmail.com')
+    embed.add_field(name='Contact',
+                    value='For feedback and new ideas you can contact me via e-mail '
+                          'mdjnunez9706@gmail.com',
+                    inline=False)
 
     embed.set_thumbnail(url=bot.user.avatar_url)
 
